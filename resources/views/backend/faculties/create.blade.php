8aa0f12c4c326ff39b0301d7b445c5ce3953f941
thanhcr7 <thanhreus2000@gmail.com>
2022-01-08 16:43:52 +0700

    use Form::model general

--- resources/views/backend/faculties/create.blade.php
+++ resources/views/backend/faculties/create.blade.php
@@ -1,25 +1,14 @@
 @extends('backend.layouts.main')
 
 @section('content')
-    <div class="content col-8">
+    <div class="content col-10">
         <div class="row flex">
             <h2>Add Faculty</h2>
         </div>
         <div class="table">
-                {{ Form::open(['method'=>'POST', 'route' => 'faculties.store' ]) }}
-                <div class="row form-group">
-                    <div class="col">
-                        {{ Form::label('', 'Name') }}
-                        {{ Form::text('name','', ['class' => 'form-control']) }}
-                        @if ( $errors->has('name') )
-                            <span class="invalid-feedback" role="alert" style="color:red;">{{ $errors->first('name') }}</span>
-                        @endif
-                    </div>
-
-                </div>
-                {{ Form::submit('Add', ['class' => 'btn btn-primary']) }}
-                <a href="{{ route('faculties.index') }}" class="btn btn-default btn-primary">Cancel</a>
-                {{ Form::close() }}
+            {!! Form::open(['action' => 'FacultyController@store']) !!}
+            @include('backend.faculties.form', ['submitButtonText' => 'Add'])
+            {!! Form::close() !!}
         </div>
     </div>
     </main>
--- resources/views/backend/faculties/edit.blade.php
+++ resources/views/backend/faculties/edit.blade.php
@@ -1,25 +1,18 @@
 @extends('backend.layouts.main')
 
 @section('content')
-    <div class="content col-8">
+    <div class="content col-10">
         <div class="row flex">
             <h2>Edit information</h2>
         </div>
         <div class="table">
-            {{ Form::open(['method'=>'PUT', 'route' => ['faculties.update', $faculty->id]]) }}
-            <div class="row form-group">
-                <div class="col">
-                    {{ Form::label('', 'Name') }}
-                    {{ Form::text('name', $faculty->name , ['class' => 'form-control']) }}
-                    @if ( $errors->has('name') )
-                        <span class="invalid-feedback" role="alert" style="color:red;">{{ $errors->first('name') }}</span>
-                    @endif
-                </div>
-            </div>
-                {{ Form::submit('Save', ['class' => 'btn btn-primary']) }}
-                <a href="{{ route('faculties.index') }}" class="btn btn-default btn-primary">Cancel</a>
-             {{ Form::close() }}
+            {!! Form::model($faculty, ['method' => 'PATCH', 'action' => ['FacultyController@update',$faculty->id]]) !!}
+            @include('backend.faculties.form', ['submitButtonText' => 'Save'])
+            {!! Form::close() !!}
         </div>
     </div>
     </main>
 @endsection
+
+
+
