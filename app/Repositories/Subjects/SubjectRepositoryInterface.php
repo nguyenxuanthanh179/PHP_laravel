b5a49b577cd2ae5eceb2b6e0cee35f67bfa6a509
thanhcr7 <thanhreus2000@gmail.com>
2022-01-11 10:27:55 +0700

    Add Subject Repository Interface

---
+++ app/Repositories/Subjects/SubjectRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Repositories\Subjects;
+
+use App\Repositories\RepositoryInterface;
+
+interface SubjectRepositoryInterface extends RepositoryInterface
+{
+    public function getSubject();
+}
