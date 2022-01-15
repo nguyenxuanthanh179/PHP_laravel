303bec96a59800a3a19a80962a7034173feb6095
thanhcr7 <thanhreus2000@gmail.com>
2022-01-15 16:17:14 +0700

    fixbug validate

--- app/Http/Requests/FacultyRequest.php
+++ app/Http/Requests/FacultyRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class FacultyRequest extends FormRequest
 {
@@ -24,7 +25,11 @@ class FacultyRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:faculties|max:50'
+            'name' => [
+                'required',
+                'max:50',
+                Rule::unique('faculties')->ignore($this->id)
+            ]
         ];
     }
 }
--- app/Http/Requests/SubjectRequest.php
+++ app/Http/Requests/SubjectRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class SubjectRequest extends FormRequest
 {
@@ -24,7 +25,11 @@ class SubjectRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:subjects|max:255'
+            'name' => [
+                'required',
+                'max:255',
+                Rule::unique('subjects')->ignore($this->id)
+            ]
         ];
     }
 }
