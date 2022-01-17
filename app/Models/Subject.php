e1f3dfec269be978300aad48ce421bd6ee42aeba
thanhcr7 <thanhreus2000@gmail.com>
2022-01-17 18:06:36 +0700

    add relationship

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     protected $fillable = ['name'];
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class,'student_subject', 'subject_id', 'student_id');
+    }
 }
